e9dd672689e53ffae251f1dc74f0e09b5f303c69
lucasjandrey@gmail.com <budk2323>
2018-09-29 16:38:05 -0300

    post

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { resolve, reject } from 'q';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  resultado = 0;
+
+  calcula() {
+    let valor = 100;
+    let valorCorrigido = 0;
+
+   // this.calcularJuros(valor);
+   // this.calcularMulta(valor);
+    this.calculaJuros(valor)
+      .then((result: number) => {
+        valorCorrigido = result;
+      this.calculaMulta(valor)
+        .then((result2: number) => {
+          valorCorrigido = result2;
+          this.resultado = valorCorrigido;
+        });
+      });
+  }
+
+  calculaJuros(valorBase: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      if (valorBase > 0) {
+        let result1 = 0;
+        let juros = 0.1;
+
+        result = valorBase + (valorBase * juros);
+        resolve(result);
+      } else {
+        reject('O valor deve ser maior que zero');
+      }
+    });
+  }
+  calculaMulta(valorBase: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      if (valorBase > 0) {
+        let result1 = 0;
+        let juros = 0.1;
+
+        result = valorBase + (valorBase * juros);
+        resolve(result);
+      }else{
+        reject('O valor deve ser maior que zero');
+      }
+    });
+  }
 }
